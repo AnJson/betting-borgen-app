560a845d97b79642a84c22afb436fe217a76bd44
Anders Jonsson <aj224rj@student.lnu.se>
2022-11-06 22:50:48 +0100

    Fix lint-config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +24,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
